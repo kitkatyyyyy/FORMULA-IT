2f0632672d5430d3d8c4f20885cccc40f7ca3492
kitkatyyyyy <139455445+kitkatyyyyy@users.noreply.github.com>
2023-10-13 17:03:50 +0300

    Add files via upload

---
+++ лабораторные работ1/Задание 2.py
@@ -0,0 +1,6 @@
+list_players = ["Маша", "Петя", "Саша", "Оля", "Кирилл", "Коля"]
+len_=len(list_players)
+step_slice1 =list_players[0:3]
+step_slice2 =list_players[3:6]
+print(step_slice1)
+print(step_slice2)
