679d70ce1fdd814bb6a8ce10ee0a63bb9ab36059
kitkatyyyyy <139455445+kitkatyyyyy@users.noreply.github.com>
2023-10-15 17:58:39 +0300

    Update Задание 3.py

--- лабораторные работ1/Задание 3.py
+++ лабораторные работ1/Задание 3.py
@@ -1,9 +1,9 @@
 # TODO Найдите количество книг, которое можно разместить на дискете
-value_simbol_in_strok=25*4
-value_strok_in_page=value_simbol_in_strok*50
-value_page_in_book=value_strok_in_page*100
-mb1=1024*1024
-all_value=(1.44*mb1)/value_page_in_book
-book=round(all_value)
+value_simbol_in_strok = 25*4
+value_strok_in_page = value_simbol_in_strok*50
+value_page_in_book = value_strok_in_page*100
+mb1 = 1024*1024
+all_value = (1.44*mb1)/value_page_in_book
+book = round(all_value)
 
 print("Количество книг, помещающихся на дискету:",book)
