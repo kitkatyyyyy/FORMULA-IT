600951f892555f372b0655b04cb121020ced06ce
kitkatyyyyy <139455445+kitkatyyyyy@users.noreply.github.com>
2023-11-01 01:28:05 +0300

    Update задание2.py

--- лаба 3/задание2.py
+++ лаба 3/задание2.py
@@ -1,20 +1,14 @@
+# TODO Напишите функцию find_common_participants
+def find_common_participants(first_, second_, separator = ','):
+    first_usual = first_.split(separator)
+    second_usual = second_.split(separator)
+    intersection_list = list(set(first_usual).intersection(second_usual))
+    intersection_list.sort()
+    return intersection_list
 
-def get_find_common_participants(first_group, second_group, sep):
-    participants_list1 = first_group.split(sep)
-    participants_list2 = second_group.split(sep)
-
-    common_participants = []
-
-    for i in participants_list1:
-        for j in participants_list2:
-            if i == j:
-                common_participants.append(i)
-                break
-
-    return common_participants
-
-# Пример использования функции:
 participants_first_group = "Иванов|Петров|Сидоров"
 participants_second_group = "Петров|Сидоров|Смирнов"
-common_participants = get_find_common_participants(participants_first_group, participants_second_group, '|')
-print(common_participants)
+
+# TODO Провеьте работу функции с разделителем отличным от запятой
+print(find_common_participants(participants_first_group, participants_second_group, ))
+
