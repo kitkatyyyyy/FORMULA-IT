af50cfd933b540c7fabf011e49ac5a4839fd9b06
kitkatyyyyy <139455445+kitkatyyyyy@users.noreply.github.com>
2023-11-15 21:38:29 +0300

    Update Задание 2.py

--- Лаба 4/Задание 2.py
+++ Лаба 4/Задание 2.py
@@ -5,11 +5,7 @@ import json
 def task(file_name: str) -> float:
     with open(file_name, 'r') as file:
         data = json.load(file)
-        total_sum = 0
-        for item in data:
-            score = item['score']
-            weight = item['weight']
-            total_sum += score * weight
+        total_sum = sum(item['score'] * item['weight'] for item in data)
 
         return round(total_sum, 3)
 
