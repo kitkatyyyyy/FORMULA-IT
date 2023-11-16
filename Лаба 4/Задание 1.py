6b515e2302aa5a603920185c8aa07bbd7da6b1bd
kitkatyyyyy <139455445+kitkatyyyyy@users.noreply.github.com>
2023-11-16 11:19:06 +0300

    Update Задание 1.py

--- Лаба 4/Задание 1.py
+++ Лаба 4/Задание 1.py
@@ -1,27 +1,27 @@
-
 # TODO импортировать необходимые молули
 
-import json
 import csv
-
+import json
 
 INPUT_FILENAME = "input.csv"
 OUTPUT_FILENAME = "output.json"
 
 
-# TODO считать содержимое csv файла
 def task(csv_file: str, delimiter: str = ',') -> str:
-    json_data = []
+    # TODO считать содержимое csv файла
+    # json_data = []
+    #
+    # with open(csv_file, 'r') as file:
+    #     reader = csv.DictReader(file, delimiter=delimiter)
+    #     for row in reader:
+    #         json_data.append(row)
+    json_data = [row for row in csv.DictReader(open(csv_file, 'r'), delimiter=delimiter)]
 
-    with open(csv_file, 'r') as file:
-        reader = csv.DictReader(file, delimiter=delimiter)
-
-        for row in reader:
-            json_data.append(row)
-# TODO Сериализовать в файл с отступами равными 4
+    # TODO Сериализовать в файл с отступами равными 4
     with open(OUTPUT_FILENAME, 'w') as output_file:
         json.dump(json_data, output_file, indent=4)
     return json.dumps(json_data, indent=4)
+
 
 if name == 'main':
     # Нужно для проверки
